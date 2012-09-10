058546bb05bfe5756d288ee8c19bd787ae466962
Mutuh <sirmutuh@gmail.com>
2012-09-10 18:14:12 +0300

    pagination refactoring upd #2

--- Tests/Controller/CategoryControllerTest.php
+++ Tests/Controller/CategoryControllerTest.php
@@ -2,21 +2,17 @@
 
 namespace Stfalcon\Bundle\PortfolioBundle\Tests\Controller;
 
-//use Stfalcon\Bundle\BlogBundle\Tests\Controller\AbstractTestCase;
-//use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Application\Bundle\DefaultBundle\Tests\Controller\AbstractTestCase;
 
 /**
  * Test cases for CategoryController
  */
-class CategoryControllerTest extends AbstractTestCase
-{
+class CategoryControllerTest extends AbstractTestCase {
 
-    public function testViewCategory()
-    {
+    public function testViewCategory() {
         $this->loadFixtures(array('Stfalcon\Bundle\PortfolioBundle\DataFixtures\ORM\LoadCategoryData'));
         $crawler = $this->fetchCrawler(
-            $this->getUrl('portfolio_category_view', array('slug' => 'web-development')), 'GET', true, true
+                $this->getUrl('portfolio_category_view', array('slug' => 'web-development')), 'GET', true, true
         );
 
         $this->assertEquals(1, $crawler->filter('html:contains("Web Development")')->count());
@@ -24,8 +20,7 @@ class CategoryControllerTest extends AbstractTestCase
         $this->assertEquals(1, $crawler->filter('html:contains("' . $description . '")')->count());
     }
 
-    public function testViewNonExistCategory()
-    {
+    public function testViewNonExistCategory() {
         $this->loadFixtures(array());
         $client = $this->makeClient(true);
         $crawler = $client->request('GET', $this->getUrl('portfolio_category_view', array('slug' => 'web-design')));
@@ -34,26 +29,13 @@ class CategoryControllerTest extends AbstractTestCase
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 
-    public function testPortfolioPagination()
-    {
+    public function testPortfolioPagination() {
         $this->loadFixtures(array(
             'Stfalcon\Bundle\PortfolioBundle\DataFixtures\ORM\LoadCategoryData',
             'Stfalcon\Bundle\PortfolioBundle\DataFixtures\ORM\LoadProjectData'
         ));
 
         $this->paginationCheck('portfolio_category_view', 'slug', 'web-development', 'project-thumb', 6);
-
-        /*$crawler = $this->fetchCrawler(
-            $this->getUrl('portfolio_category_view', array('slug' => 'web-development')), 'GET', true, true
-        );
-        $this->assertEquals(1, $crawler->filter('.pagination .current:contains("1")')->count());
-        $this->assertEquals(6, $crawler->filter('img.project-thumb')->count());
-
-        $crawler = $this->fetchCrawler(
-            $this->getUrl('portfolio_category_view', array('slug' => 'web-development', 'page'=> 2)), 'GET', true, true
-        );
-        $this->assertEquals(1, $crawler->filter('.pagination .current:contains("2")')->count());
-        $this->assertEquals(2, $crawler->filter('img.project-thumb')->count());*/
     }
 
 }
